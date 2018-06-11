b009daad841eccc75ef9eb614c07bca91f1d077c
paulberthon <39916733+paulberthon@users.noreply.github.com>
2018-06-11 16:09:49 +0200

    Update Pce.py

--- software/openvisualizer/openvisualizer/Pce/Pce.py
+++ software/openvisualizer/openvisualizer/Pce/Pce.py
@@ -4,16 +4,15 @@ import
 
 
 class PCE:
-  def __init__(self):
-    self.ADDRESSES= []
-    self.NeighboursRow =[]
-    self.NumTx=[]
-    self.NumTxACk=[]
+   ADDRESSES= []
+   NeighboursRow =[]
+   NumTx=[]
+   NumTxACk=[]
   
   def GetID(self,Addr):
     k=0
-    for i in range(len(self.ADDRESSES)-1)
-      if self.ADDRESSES[i]==Addr
+    for i in range(len(self.ADDRESSES)-1):
+      if (self.ADDRESSES[i]==Addr):
         return i
     raise SystemError('Unable to find Mote')
     
@@ -33,7 +32,7 @@ class PCE:
     
   
  def update(self,Addr,Neighbours,NumTx,NumTxACK)
-    i=get_ID(Addr)
+    i=self.get_ID(Addr)
     self.NeighboursRow[i]=Neighbours
     self.NumTx[i]+=NumTx
     self.NumTxACK[i]+=NumTxACK
