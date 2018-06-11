b3611282831e7385a75779d83298f4da513bce42
paulberthon <39916733+paulberthon@users.noreply.github.com>
2018-06-11 14:18:38 +0200

    Update Pce.py

--- software/openvisualizer/openvisualizer/Pce/Pce.py
+++ software/openvisualizer/openvisualizer/Pce/Pce.py
@@ -10,29 +10,29 @@ class PCE:
     self.NumTx=[]
     self.NumTxACk=[]
   
-  def GetID(Addr):
+  def GetID(self,Addr):
     k=0
     for i in range(len(self.ADDRESSES)-1)
       if self.ADDRESSES[i]==Addr
         return i
     raise SystemError('Unable to find Mote')
     
-  def GetPDR(Addr)
+  def GetPDR(self,Addr)
     i=get_ID(Addr)
     return(self.NumTxACK[i]/self.NumTx[i])
     
-  def GetNeighbours(Addr)
+  def GetNeighbours(self,Addr)
     i=get_ID(Addr)
     return(self.NeigboursRow[i])
            
-  def add(Addr,Neighbours,NumTx,NumTxACK)
+  def add(self,Addr,Neighbours,NumTx,NumTxACK)
     self.self.ADDRESSES.append(Addr)
     self.NeighboursRow.append(Neighbours)
     self.NumTx.append(NumTx)
     self.NumTxACk.append(NumTxACK)
     
   
- def update(Addr,Neighbours,NumTx,NumTxACK)
+ def update(self,Addr,Neighbours,NumTx,NumTxACK)
     i=get_ID(Addr)
     self.NeighboursRow[i]=Neighbours
     self.NumTx[i]+=NumTx
