2ada4feae7332a3c79a4c7de9e9e56f30da3108e
paulberthon <39916733+paulberthon@users.noreply.github.com>
2018-06-13 16:34:20 +0200

    Update Pce.py

--- software/openvisualizer/openvisualizer/Pce/Pce.py
+++ software/openvisualizer/openvisualizer/Pce/Pce.py
@@ -1,3 +1,6 @@
+
+import json
+
 from openvisualizer.moteState import moteState
 
 
@@ -8,35 +11,45 @@ class PCE:
    NeighboursRow =[]
    NumTx=[]
    NumTxACk=[]
+   PDR=[)
   
-  def GetID(self,Addr):
+  def getID(self,moteState.ms):
     k=0
-    for i in range(len(self.ADDRESSES)-1):
+    Addr=ms.getStateElem(ms.ST_IDMANAGER)
+    for i in range(len(self.ADDRESSES)):
       if (self.ADDRESSES[i]==Addr):
         return i
     raise SystemError('Unable to find Mote')
     
-  def GetPDR(self,Addr)
-    i=get_ID(Addr)
+  def GetPDR(self,moteState.ms)
+    i=self.getID(ms)
     return(self.NumTxACK[i]/self.NumTx[i])
     
-  def GetNeighbours(self,Addr)
-    i=get_ID(Addr)
+  def GetNeighbours(self,moteState.ms)
+    i=self.getID(ms)
     return(self.NeigboursRow[i])
            
-  def add(self,moteState.moteState)
-    self.ADDRESSES.append(Addr)
-    self.NeighboursRow.append(Neighbours)
-    self.NumTx.append(NumTx)
-    self.NumTxACk.append(NumTxACK)
+  def add(self,moteState.ms)
+    self.ADDRESSES.append(ms.getStateElem(ms.ST_IDMANAGER))
+    linkInfo=self.dataParsingNeigbours(moteState.ms)
+    self.NeighboursRow.append([linkInfo[i][0] for i in range(len(linkInfo))])
+    self.NumTx.append([linkInfo[i][1] for i in range(len(linkInfo))])
+    self.NumTxACk.append([linkInfo[i][2] for i in range(len(linkInfo))])
+    self.PDR.append([linkInfo[i][1]/linkInfo[i][2] for i in range(len(linkInfo))])
     
   
- def update(self,Addr,Neighbours,NumTx,NumTxACK)
-    i=self.get_ID(Addr)
-    self.NeighboursRow[i]=Neighbours
-    self.NumTx[i]+=NumTx
-    self.NumTxACK[i]+=NumTxACK
+ def update(self,moteState.ms)
+    i=self.getID(ms)
+    linkInfo=self.dataParsingNeigbours(moteState.ms)
+    self.NeighboursRow[i]=[linkInfo[i][0] for i in range(len(linkInfo))]
+    self.NumTx[i]=[linkInfo[i][1] for i in range(len(linkInfo))]
+    self.NumTxACK[i]=[linkInfo[i][2] for i in range(len(linkInfo))]
+    self.PDR=[linkInfo[i][1]/linkInfo[i][2] for i in range(len(linkInfo))]
   
   
-
-
+def dataParsingNeigbours(self,moteState.ms)
+   obj = json.loads(ms.getStateElem(ms.ST_NEIGHBOURS))
+   tab= []
+   for i in range(len(obj['data'])):
+      tab.append([obj['data'][i],obj['data'][i]['numTx'],obj['data'][i]['numTxACK']])
+   return tab
