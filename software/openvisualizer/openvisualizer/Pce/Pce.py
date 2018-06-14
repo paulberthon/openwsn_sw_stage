7655d4bc7d6ac52c455eea6e2662fe9483c8d112
paulberthon <39916733+paulberthon@users.noreply.github.com>
2018-06-14 16:34:28 +0200

    Update Pce.py

--- software/openvisualizer/openvisualizer/Pce/Pce.py
+++ software/openvisualizer/openvisualizer/Pce/Pce.py
@@ -1,6 +1,6 @@
 
 import json
-
+import time
 from openvisualizer.moteState import moteState
 
 
@@ -8,42 +8,45 @@ from openvisualizer.moteState import moteState
 
 class PCE:
    ADDRESSES= []
+   Motes=[]
    NeighboursRow =[]
    NumTx=[]
    NumTxACk=[]
-   PDR=[)
-  
-  def getID(self,moteState.ms):
-    k=0
-    Addr=ms.getStateElem(ms.ST_IDMANAGER)
+   PDR=[]
+   lastUpdated=[]
+  @staticmethod
+  def getID(self,Addr):
     for i in range(len(self.ADDRESSES)):
       if (self.ADDRESSES[i]==Addr):
         return i
     raise SystemError('Unable to find Mote')
   
+  @staticmethod
   def GetMoteElem(self,moteState.ms,elemName)
       i=self.getID(ms)
       return(self.elemName[i]
              
-             
+  @staticmethod            
   def add(self,moteState.ms)
     self.ADDRESSES.append(ms.getStateElem(ms.ST_IDMANAGER))
+    Motes.append(ms)
     linkInfo=self.dataParsingNeigbours(moteState.ms)
     self.NeighboursRow.append([linkInfo[i][0] for i in range(len(linkInfo))])
     self.NumTx.append([linkInfo[i][1] for i in range(len(linkInfo))])
     self.NumTxACk.append([linkInfo[i][2] for i in range(len(linkInfo))])
     self.PDR.append([linkInfo[i][1]/linkInfo[i][2] for i in range(len(linkInfo))])
-    
+    self.lastUpdated.append(time.clock())
   
- def update(self,moteState.ms)
-    i=self.getID(ms)
-    linkInfo=self.dataParsingNeigbours(moteState.ms)
+ @staticmethod
+ def update(self,Addr)
+    i=self.getID(Addr)
+    linkInfo=self.dataParsingNeigbours(Motes[i])
     self.NeighboursRow[i]=[linkInfo[i][0] for i in range(len(linkInfo))]
     self.NumTx[i]=[linkInfo[i][1] for i in range(len(linkInfo))]
     self.NumTxACK[i]=[linkInfo[i][2] for i in range(len(linkInfo))]
     self.PDR=[linkInfo[i][1]/linkInfo[i][2] for i in range(len(linkInfo))]
   
-  
+@staticmethod  
 def dataParsingNeigbours(self,moteState.ms)
    obj = json.loads(ms.getStateElem(ms.ST_NEIGHBOURS))
    tab= []
