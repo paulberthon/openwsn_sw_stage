9a4e9d94a8e1e6055b3901440be6c4649ad87dc6
paulberthon <39916733+paulberthon@users.noreply.github.com>
2018-06-18 12:24:17 +0200

    Update Pce.py

--- software/openvisualizer/openvisualizer/Pce/Pce.py
+++ software/openvisualizer/openvisualizer/Pce/Pce.py
@@ -33,16 +33,18 @@ class PCE:
       self.Addresses.append(ms.getStateElem(ms.ST_IDMANAGER))
       Motes.append(ms)
       linkInfo=self.dataParsingNeigbours(moteState.ms)
+      scheduleInfo=self.dataParsingSchedule(Motes[i])
       self.NeighboursRow.append([linkInfo[i][0] for i in range(len(linkInfo))])
       self.NumTx.append([linkInfo[i][1] for i in range(len(linkInfo))])
       self.NumTxACk.append([linkInfo[i][2] for i in range(len(linkInfo))])
       self.Pdr.append([linkInfo[i][1]/linkInfo[i][2] for i in range(len(linkInfo))])
-      self.Schedule.append([linkInfo[i][3] for i in range(len(linkInfo))])
+      self.Schedule.append([scheduleInfo[j] for j in range(len(scheduleInfo))])
   
    @staticmethod
    def update(self,Addr):
       i=self.getID(Addr)
       linkInfo=self.dataParsingNeigbours(Motes[i])
+      scheduleInfo=self.dataParsingSchedule(Motes[i])
       if len(linkInfo[i]==0):
          self.delete(Motes[i])
       else:
@@ -50,7 +52,7 @@ class PCE:
          self.NumTx[i]=[linkInfo[j][1] for j in range(len(linkInfo))]
          self.NumTxACK[i]=[linkInfo[j][2] for j in range(len(linkInfo))]
          self.Pdr[i]=[linkInfo[j][1]/linkInfo[j][2] for j in range(len(linkInfo))]
-         self.Schedule[i]=[linkInfo[j][3] for j in range(len(linkInfo))]
+         self.Schedule[i]=[scheduleInfo[j] for j in range(len(scheduleInfo))]
          
          
    @staticmethod  
@@ -58,10 +60,18 @@ class PCE:
       obj = json.loads(ms.getStateElem(ms.ST_NEIGHBOURS))
       obj2 = json.loads(ms.getStateElem(ms.ST_SCHEDULE))
       tab= []
-      k=0
-      while obj['data'][k]['Neighbors'] != ' (None)':
-         tab.append([obj['data'][i],obj['data'][i]['numTx'],obj['data'][i]['numTxACK'],obj2['data'][i]])
-         k+=1
+      for k in range(len(obj['data'])):
+         if obj['data'][k]['Neighbors'] != ' (None)':
+         tab.append([obj['data'][k],obj['data'][k]['numTx'],obj['data'][k]['numTxACK'],obj2['data'][k]])
+      return tab
+   
+   @staticmethod  
+   def dataParsingSchedule(self,moteState.ms):
+      obj = json.loads(ms.getStateElem(ms.ST_SCHEDULE))
+      tab= []
+      for k in range(len(obj['data'])):
+         if obj['data'][k]['neighbor'] != ' (None)':
+         tab.append(obj['data'][k])
       return tab
    
    
