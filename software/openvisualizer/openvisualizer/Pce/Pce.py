708bd5b8940115b10eb097e3b8d366c84847780a
paulberthon <39916733+paulberthon@users.noreply.github.com>
2018-07-02 17:08:41 +0200

    Update Pce.py

--- software/openvisualizer/openvisualizer/Pce/Pce.py
+++ software/openvisualizer/openvisualizer/Pce/Pce.py
@@ -2,7 +2,31 @@
 import json
 import time
 
-from openvisualizer.moteState     import *
+from openvisualizer.moteState     import moteState
+
+import json
+
+def dataParsingNeighbours(moteStateObject):
+    '''Uses Json to parse the data obtained via the getStateElem function from the moteState class
+    applied on the ST_NEIGHBOURS parameter'''
+    obj = json.loads(moteStateObject.getStateElem(moteStateObject.ST_NEIGHBOURS))
+    tab= []
+    for k in range(len(obj['data'])):
+        if obj['data'][k]['Neighbors'] != ' (None)':
+            tab.append([obj['data'][k],obj['data'][k]['numTx'],obj['data'][k]['numTxACK']])
+    return tab
+
+def dataParsingSchedule(moteStateObject):
+    '''Uses Json to parse the data obtained via the getStateElem function from the moteState class
+    applied on the ST_SCHEDULE parameter'''
+    obj = json.loads(moteStateObject.getStateElem(moteStateObject.ST_SCHEDULE))
+    tab= []
+    for k in range(len(obj['data'])):
+        if obj['data'][k]['neighbor'] != ' (None)':
+            tab.append(obj['data'][k])
+    return tab
+
+
 
 class Pce:
    Addresses= []       #Contains the addresses (16/32/64 bits) of all known motes and is used as a key to find their datas in other tables
@@ -14,28 +38,7 @@ class Pce:
    lastUpdated=[]
    Schedule=[]          #Contains the Scheduling tables of each motes
    memorySize=30
-
-   @staticmethod
-   def dataParsingNeigbours(moteStateObject):
-       '''Uses Json to parse the data obtained via the getStateElem function from the moteState class
-       applied on the ST_NEIGHBOURS parameter'''
-       obj = json.loads(moteStateObject.getStateElem(moteStateObject.ST_NEIGHBOURS))
-       tab= []
-       for k in range(len(obj['data'])):
-           if obj['data'][k]['Neighbors'] != ' (None)':
-               tab.append([obj['data'][k],obj['data'][k]['numTx'],obj['data'][k]['numTxACK']])
-       return tab
-
-   @staticmethod
-   def dataParsingSchedule(moteStateObject):
-       '''Uses Json to parse the data obtained via the getStateElem function from the moteState class
-       applied on the ST_SCHEDULE parameter'''
-       obj = json.loads(moteStateObject.getStateElem(moteStateObject.ST_SCHEDULE))
-       tab= []
-       for k in range(len(obj['data'])):
-           if obj['data'][k]['neighbor'] != ' (None)':
-               tab.append(obj['data'][k])
-       return tab
+   files=[]
 
 
    @classmethod
@@ -61,11 +64,11 @@ class Pce:
        del cls.motes[index]
 
    @classmethod
-   def getID(cls,Addr):
+   def getID(cls,moteStateObject):
        ''''Is used to retrive the index corresponding to a mote's address
        so that it can be used as a primary key in other tables later on'''
        for i in range(len(cls.Addresses)):
-           if (cls.Addresses[i]==Addr):
+           if (cls.Addresses[i]==moteStateObject.getStateElem(moteStateObject.ST_IDMANAGER)):
                return i
        return "Mote not found"
 
@@ -81,26 +84,27 @@ class Pce:
        It creates new fields in every tables to be completed with the new mote's datas'''
        cls.Addresses.append(moteStateObject.getStateElem(moteStateObject.ST_IDMANAGER))
        cls.motes.append(moteStateObject)
-       linkInfo=dataParsingNeigbours(moteStateObject)
+       linkInfo=dataParsingNeighbours(moteStateObject)
        scheduleInfo=dataParsingSchedule(moteStateObject)
        cls.NeighboursRow.append([linkInfo[i][0] for i in range(len(linkInfo))])
        cls.NumTx.append([linkInfo[i][1] for i in range(len(linkInfo))])
        cls.NumTxACk.append([linkInfo[i][2] for i in range(len(linkInfo))])
        cls.Pdr.append([linkInfo[i][1]/linkInfo[i][2] for i in range(len(linkInfo))])
        cls.Schedule.append([scheduleInfo[j] for j in range(len(scheduleInfo))])
+       cls.files.append(open("mote"+cls.getID(moteStateObject))+".txt","w")
 
    @classmethod
    def update(cls,moteStateObject):
        '''This function is used when a mote's datas are updated the updating the values
        contained in each table
        -It only writes the used fields
-       -When no Neighbour is detected it means that the mote is disconected and as to be deleted from
+       -When no Neighbour is detected it means that the mote is disconected and has to be deleted from
        the Network'''
-       i=cls.getID(Addr)
+       i=cls.getID(moteStateObject)
        if i== "Mote not found":
            cls.add(moteStateObject)
        else:
-           linkInfo=dataParsingNeigbours(motes[i])
+           linkInfo=dataParsingNeighbours(motes[i])
            scheduleInfo=dataParsingSchedule(motes[i])
            if len(linkInfo[i]==0):
                cls.delete(motes[i])
@@ -112,4 +116,7 @@ class Pce:
                cls.NumTxACK[i].append([linkInfo[j][2] for j in range(len(linkInfo))])
                cls.Pdr[i].append([linkInfo[j][1]/linkInfo[j][2] for j in range(len(linkInfo))])
                cls.Schedule[i].append([scheduleInfo[j] for j in range(len(scheduleInfo))])
-       print(cls.Schedule[i])
+               write(cls.files[i]("\n"+"update time:"+time.time()+cls.Addresses+"\n"+cls.NeighboursRow[i]+"\n"+cls.NumTx[i]+"\n"+cls.NumTxACK[i]+"\n"+cls.Schedule[i]+"\n"+"Pdr:"cls.Pdr[i]+"\n"))
+
+    def __init__():
+        for file in files:
